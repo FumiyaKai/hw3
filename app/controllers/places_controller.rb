4708a955090852e80842dde91af6d45142b6dd18
Fumiya Kai <fumiya.kai@kellogg.northwestern.edu>
2023-05-16 02:03:58 +0000

    commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,21 @@
 class PlacesController < ApplicationController
   def index
     @places = Place.all
-    render :template => "places/index"
+    # render :template => "places/index"
   end
+
+  def show
+    @place = Place.find_by({"id" => params["id"]})
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new
+    @place["name"] = params["place"]["name"]
+    @place.save
+  end
+
 end
