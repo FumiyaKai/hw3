63b0723860f90ed54ed104cdbb0484887257ec78
Fumiya Kai <fumiya.kai@kellogg.northwestern.edu>
2023-05-16 02:57:29 +0000

    commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,15 @@ class PostsController < ApplicationController
     render :template => "posts/index"
   end
   
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Place.new
+    @post["name"] = params["post"]["name"]
+    @post.save
+    redirect_to "/posts"
+  end
+
 end
